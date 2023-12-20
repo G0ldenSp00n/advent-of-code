4be10f04e10b7ef57de0df8d28ca1fb8a91afaed
G0ldenSp00n <hayfithyan+github.com/G0ldenSp00n@gmail.com>
2023-12-20 01:06:32 -0800

    2023: day-9

---
+++ 2023/day-9/src/bin/challenge1.rs
@@ -0,0 +1,57 @@
+use nom::{
+    character::complete::{i64, multispace1},
+    multi::separated_list1,
+    IResult,
+};
+
+fn find_diff(input: Vec<i64>) -> Vec<Vec<i64>> {
+    let mut all_zero = true;
+    let res: Vec<i64> = input
+        .iter()
+        .zip(input.iter().skip(1))
+        .map(|(a, b)| {
+            println!("a {a}, b {b}");
+            let res = b - a;
+            if res != 0 {
+                all_zero = false;
+            }
+            res
+        })
+        .collect();
+
+    let mut output: Vec<Vec<i64>> = vec![input.clone()];
+    if !all_zero {
+        let mut diff = find_diff(res);
+        output.append(&mut diff);
+    }
+    output
+}
+
+fn calculate_next_seq(input: Vec<i64>) -> i64 {
+    let diff = find_diff(input);
+    diff.iter().rev().fold(0, |acc, curr| {
+        let last = curr.last().unwrap();
+        acc + last
+    })
+}
+
+fn parse_vec(input: &str) -> IResult<&str, Vec<i64>> {
+    let (input, u64vecvec) = separated_list1(multispace1, i64)(input)?;
+    Ok((input, u64vecvec))
+}
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let res: i64 = input
+        .split("\n")
+        .filter(|str| !str.is_empty())
+        .map(|input| {
+            let (_, data) = parse_vec(input).expect("Should parse correctly");
+            println!("{data:?}");
+            let o = calculate_next_seq(data);
+            println!("{o:?}");
+            o
+        })
+        .sum();
+    println!("{res}");
+}
