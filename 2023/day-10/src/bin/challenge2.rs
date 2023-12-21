10597ad1faccd63cee353b33085c7c165cb0b755
G0ldenSp00n <hayfithyan+github.com/G0ldenSp00n@gmail.com>
2023-12-21 01:36:38 -0800

    2023: day-10 - part two (wip)

--- 2023/day-10/src/bin/challenge2.rs
+++ 2023/day-10/src/bin/challenge2.rs
@@ -1,6 +1,6 @@
 use std::char;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 enum Tile {
     VerticalPipe,
     HorizontalPipe,
@@ -12,6 +12,21 @@ enum Tile {
     StartingPosition,
 }
 
+impl Tile {
+    fn to_char(&self) -> char {
+        match self {
+            Self::VerticalPipe => '|',
+            Self::HorizontalPipe => '-',
+            Self::NorthToEastBend => 'L',
+            Self::NorthToWestBend => 'J',
+            Self::SouthToWestBend => '7',
+            Self::SouthToEastBend => 'F',
+            Self::Ground => '.',
+            Self::StartingPosition => 'S',
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 struct Position {
     row_index: usize,
@@ -21,47 +36,80 @@ struct Position {
 #[derive(Debug)]
 struct PipeNetwork {
     zone: Vec<Vec<Tile>>,
-    animal: Position,
+    animal: Option<Position>,
+}
+
+enum PipeNetworkSettings<'a> {
+    Sized {
+        width: usize,
+        height: usize,
+        fill: Tile,
+    },
+    FromStr(&'a str),
 }
 
 impl PipeNetwork {
-    fn new(input: &str) -> Result<Self, &str> {
-        let mut position = None;
-        let zone = input
-            .split("\n")
-            .filter(|row| !row.is_empty())
-            .enumerate()
-            .map(|(row_index, rows)| {
-                rows.chars()
+    fn new(pipe_network_settings: PipeNetworkSettings) -> Result<Self, &str> {
+        match pipe_network_settings {
+            PipeNetworkSettings::Sized {
+                width,
+                height,
+                fill,
+            } => Ok(PipeNetwork {
+                zone: vec![vec![fill; width]; height],
+                animal: None,
+            }),
+            PipeNetworkSettings::FromStr(input) => {
+                let mut position = None;
+                let zone = input
+                    .split("\n")
+                    .filter(|row| !row.is_empty())
                     .enumerate()
-                    .map(|(col_index, char)| {
-                        if char == 'S' {
-                            position = Some(Position {
-                                row_index,
-                                col_index,
-                            });
-                        }
-                        match char {
-                            '|' => Tile::VerticalPipe,
-                            '-' => Tile::HorizontalPipe,
-                            'L' => Tile::NorthToEastBend,
-                            'J' => Tile::NorthToWestBend,
-                            '7' => Tile::SouthToWestBend,
-                            'F' => Tile::SouthToEastBend,
-                            '.' => Tile::Ground,
-                            'S' => Tile::StartingPosition,
-                            _ => panic!(),
-                        }
+                    .map(|(row_index, rows)| {
+                        rows.chars()
+                            .enumerate()
+                            .map(|(col_index, char)| {
+                                if char == 'S' {
+                                    position = Some(Position {
+                                        row_index,
+                                        col_index,
+                                    });
+                                }
+                                match char {
+                                    '|' => Tile::VerticalPipe,
+                                    '-' => Tile::HorizontalPipe,
+                                    'L' => Tile::NorthToEastBend,
+                                    'J' => Tile::NorthToWestBend,
+                                    '7' => Tile::SouthToWestBend,
+                                    'F' => Tile::SouthToEastBend,
+                                    '.' => Tile::Ground,
+                                    'S' => Tile::StartingPosition,
+                                    _ => panic!(),
+                                }
+                            })
+                            .collect()
                     })
-                    .collect()
-            })
-            .collect();
+                    .collect();
 
-        if let Some(animal) = position {
-            Ok(PipeNetwork { zone, animal })
-        } else {
-            Err("no animal")
+                if let Some(animal) = position {
+                    Ok(PipeNetwork {
+                        zone,
+                        animal: Some(animal),
+                    })
+                } else {
+                    Err("no animal")
+                }
+            }
         }
+    }
+
+    fn draw(&self) {
+        self.zone.iter().for_each(|tiles| {
+            tiles
+                .iter()
+                .for_each(|tile| print!("{}", tile.to_char().to_string()));
+            println!();
+        })
     }
 
     fn follow_pipe(&self, position: Position, prev_position: Position) -> Position {
@@ -245,8 +293,9 @@ impl PipeNetwork {
     }
 
     fn find_furthest_point(&self) -> i32 {
-        let mut prev_position = self.animal;
-        let mut next_position = self.follow_pipe(self.animal, self.animal);
+        let animal = self.animal.unwrap();
+        let mut prev_position = animal;
+        let mut next_position = self.follow_pipe(animal, animal);
         let mut length = 1;
         while self
             .zone
@@ -263,11 +312,101 @@ impl PipeNetwork {
         }
         length / 2
     }
+
+    fn get_tile(&self, position: Position) -> Tile {
+        self.zone
+            .get(position.row_index)
+            .unwrap()
+            .get(position.col_index)
+            .unwrap()
+            .clone()
+    }
+
+    fn set_tile(&mut self, position: Position, tile: Tile) {
+        self.zone[position.row_index][position.col_index] = tile;
+    }
+
+    fn copy_tile(&self, pipe_network: &mut PipeNetwork, position: Position) {
+        pipe_network.set_tile(position, self.get_tile(position));
+    }
+
+    fn flood_fill(&mut self, position: Position) {
+        self.set_tile(position, Tile::StartingPosition);
+        for row_offset in -1..=1 {
+            for col_offset in -1..=1 {
+                if let Ok(col_index) =
+                    usize::try_from(isize::try_from(position.col_index).unwrap() + col_offset)
+                {
+                    if let Ok(row_index) =
+                        usize::try_from(isize::try_from(position.row_index).unwrap() + row_offset)
+                    {
+                        if let Some(row) = self.zone.get(row_index) {
+                            if let Some(tile) = row.get(col_index) {
+                                if tile == &Tile::Ground {
+                                    self.flood_fill(Position {
+                                        row_index,
+                                        col_index,
+                                    })
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn count_tile(&self, tile: Tile) -> u32 {
+        let mut count = 0;
+        self.zone.iter().for_each(|row| {
+            row.iter().for_each(|zone_tile| {
+                if zone_tile == &tile {
+                    count += 1
+                }
+            })
+        });
+        count
+    }
+
+    fn trace_edges(&self) -> Self {
+        let mut pipe_network = Self::new(PipeNetworkSettings::Sized {
+            width: self.zone.get(0).unwrap().len(),
+            height: self.zone.len(),
+            fill: Tile::Ground,
+        })
+        .unwrap();
+
+        let animal = self.animal.unwrap();
+        let mut prev_position = animal;
+        let mut next_position = self.follow_pipe(animal, animal);
+        while self
+            .zone
+            .get(next_position.row_index)
+            .unwrap()
+            .get(next_position.col_index)
+            .unwrap()
+            != &Tile::StartingPosition
+        {
+            let new_position = self.follow_pipe(next_position, prev_position);
+            self.copy_tile(&mut pipe_network, new_position);
+            prev_position = next_position;
+            next_position = new_position;
+        }
+
+        pipe_network
+    }
 }
 
 fn main() {
-    let input = include_str!("input1.txt");
-    let pipe_network = PipeNetwork::new(input).expect("Should be a valid network");
-    let result = pipe_network.find_furthest_point();
+    let input = include_str!("input2.txt");
+    let pipe_network =
+        PipeNetwork::new(PipeNetworkSettings::FromStr(input)).expect("Should be a valid network");
+    let mut new_pipe_network = pipe_network.trace_edges();
+    new_pipe_network.flood_fill(Position {
+        row_index: 0,
+        col_index: 0,
+    });
+    new_pipe_network.draw();
+    let result = new_pipe_network.count_tile(Tile::Ground);
     println!("{result}");
 }
